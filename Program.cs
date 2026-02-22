ab644207db485b14445ecb8b18513e9d59c13443
emanu <emanu@EMANUEL_RAMREZ>
2026-02-21 19:20:02 -0600

    Menu principal

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
 ﻿//1.) Triaje médico y prioridad de atención
+Console.WriteLine("MENU");
+Console.WriteLine("Presione 1 para Emergencia");
+Console.WriteLine("Presione 2 para Consulta");
+Console.WriteLine("Presione 3 para Pediatría");
+Console.WriteLine("Presione 4 para Traumatología");
