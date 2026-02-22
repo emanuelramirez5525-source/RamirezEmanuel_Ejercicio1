f483c77afa85221a0f15aede46e8bf7a9b702d24
emanu <emanu@EMANUEL_RAMREZ>
2026-02-21 19:22:37 -0600

    Switch implementado

--- Program.cs
+++ Program.cs
@@ -4,3 +4,36 @@ Console.WriteLine("Presione 1 para Emergencia");
 Console.WriteLine("Presione 2 para Consulta");
 Console.WriteLine("Presione 3 para Pediatría");
 Console.WriteLine("Presione 4 para Traumatología");
+
+int opcion = int.Parse(Console.ReadLine());
+
+switch (opcion)
+{
+    case 1:
+
+        break;
+
+
+     case 2: 
+        
+        
+        
+        break;
+
+
+        case 3:
+
+
+        break;
+
+        case 4:
+
+
+        break;
+
+
+    default:
+        Console.WriteLine("Opción ingresada incorrecta");
+        break;
+
+}
